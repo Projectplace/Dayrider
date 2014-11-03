8e1b3589cc8a857c9324a751db76c699ae91e95e
Stefan Lundström <stefanl@projectplace.com>
2014-11-03 16:30:12 +0100

    Changed mailbox api to be just how the user presses

--- dayrider/dayrider.ino
+++ dayrider/dayrider.ino
@@ -9,14 +9,14 @@ Dagu4Motor left(11, 12, 0, 0, 0);
 Dagu4Motor right(3, 2, 0, 0, 0);
 
 void setup() {
-  Serial.begin(9600);
+  //Serial.begin(9600);
   pinMode(13,OUTPUT);
   digitalWrite(13, LOW);
   Bridge.begin();
   Mailbox.begin();
   digitalWrite(13, HIGH);
   
-  Serial.println("Starting Dayrider 0.1");
+  //Serial.println("Starting Dayrider 0.1");
   
   left.begin();
   left.stopMotors();
@@ -25,31 +25,98 @@ void setup() {
 
 void loop() {
   if (Mailbox.messageAvailable()) {
-    process_mb();
+    process_cmd();
   }
 
   delay(50); 
 }
 
-void process_mb() {
-  int start, end, dir, spd;
-  String msg, side, tmp;
+#define LEFT 1
+#define RIGHT 2
+#define FORWARD 3
+#define BACKWARD 4
+
+#define MOTOR_FORWARD 0
+#define MOTOR_BACKWARD 1
+
+#define MOTOR_SPEED 150
+
+int current_direction = 0;
+
+void process_cmd() {
+  String cmd;
   
-  Mailbox.readMessage(msg);
+  Mailbox.readMessage(cmd);
+  if (cmd == "left") {
+    turn_left();
+  } else if (cmd == "right") {
+    turn_right();
+  } else if (cmd == "forward") {
+    go_forward();
+  } else if (cmd == "backward") {
+    go_backwards();
+  }
 
-  Serial.println(msg);
+  //Serial.println(cmd);
 
-  end = msg.indexOf('/');
-  side = msg.substring(0, end);
-  start = end+1;
-  end = msg.indexOf('/', start);
-  dir = msg.substring(start, end).toInt();
-  start = end+1;
-  spd = msg.substring(start).toInt();
-
-  motorCommand(side, dir, spd);  
 }
 
+void turn_left() {
+     if (current_direction == RIGHT) {
+        stop_movement();
+        current_direction = 0;
+        return;
+     }  
+     left.setMotorDirection(MOTOR_FORWARD);
+     left.setSpeed(MOTOR_SPEED);   
+     //right.setMotorDirection(MOTOR_BACKWARD);
+     //right.setSpeed(MOTOR_SPEED / 2);
+     current_direction = LEFT;
+}
+
+void turn_right() {
+     if (current_direction == LEFT) {
+        stop_movement();
+        current_direction = 0;
+        return;
+     }
+     //right.setMotorDirection(MOTOR_FORWARD);
+     //right.setSpeed(MOTOR_SPEED);   
+     left.setMotorDirection(MOTOR_BACKWARD);
+     left.setSpeed(MOTOR_SPEED / 2);   
+     current_direction = RIGHT;
+}
+
+void go_forward() {
+     if (current_direction == BACKWARD) {
+        stop_movement();
+        current_direction = 0;
+        return;
+     }
+     //right.setMotorDirection(MOTOR_FORWARD);
+     //right.setSpeed(MOTOR_SPEED);   
+     left.setMotorDirection(MOTOR_FORWARD);
+     left.setSpeed(MOTOR_SPEED);   
+     current_direction = FORWARD;
+}
+
+void go_backwards() {
+     if (current_direction == FORWARD) {
+        stop_movement();
+        current_direction = 0;
+        return;
+     }
+     //right.setMotorDirection(MOTOR_BACKWARD);
+     //right.setSpeed(MOTOR_SPEED);   
+     left.setMotorDirection(MOTOR_BACKWARD);
+     left.setSpeed(MOTOR_SPEED);   
+     current_direction = BACKWARD;
+}
+
+void stop_movement() {
+     //right.setSpeed(0);   
+     left.setSpeed(0); 
+}
 
 void motorCommand(String side, int dir, int spd) {
   
